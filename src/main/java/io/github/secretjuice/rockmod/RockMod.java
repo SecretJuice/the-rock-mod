80405ca1f732c1a91b65b999cbb5405f2379fd57
SecretJuice <secret.juice33@gmail.com>
2021-02-10 15:13:35 -0600

    Added Creative Tab, Added better granite rock texture

--- src/main/java/io/github/secretjuice/rockmod/RockMod.java
+++ src/main/java/io/github/secretjuice/rockmod/RockMod.java
@@ -3,6 +3,8 @@ package io.github.secretjuice.rockmod;
 import io.github.secretjuice.rockmod.core.dispenserbehaviors.RockDispenserBehavior;
 import io.github.secretjuice.rockmod.core.init.ItemInit;
 import io.github.secretjuice.rockmod.network.handler.RockModPacketHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -17,6 +19,7 @@ public class RockMod
 
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "sj_rock_mod";
+    public static final ItemGroup ROCK_GROUP = new RockItemGroup("sj_rock_mod_tab");
 
     public RockMod() {
 
@@ -32,6 +35,19 @@ public class RockMod
     {
         RockModPacketHandler.register();
         RockDispenserBehavior.init();
+    }
+
+    public static class RockItemGroup extends ItemGroup {
+        public RockItemGroup(String label) {
+            super(label);
+        }
+
+        @Override
+        public ItemStack createIcon(){
+            return ItemInit.ROCK.get().getDefaultInstance();
+        }
+
+
     }
 
 }
--- src/main/java/io/github/secretjuice/rockmod/core/init/ItemInit.java
+++ src/main/java/io/github/secretjuice/rockmod/core/init/ItemInit.java
@@ -14,26 +14,26 @@ public class ItemInit {
             RockMod.MOD_ID);
 
     public static final RegistryObject<RockItem> ROCK = ITEMS.register("rock",
-            () -> new RockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new RockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<MagmaRockItem> MAGMA_ROCK = ITEMS.register("magmarock",
-            () -> new MagmaRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new MagmaRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<EndRockItem> END_ROCK = ITEMS.register("endrock",
-            () -> new EndRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new EndRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<ObsidianRockItem> OBSIDIAN_ROCK = ITEMS.register("obsidianrock",
-            () -> new ObsidianRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new ObsidianRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<BobRocksItem> BOB_ROCKS = ITEMS.register("bobrocks",
-            () -> new BobRocksItem(new Item.Properties().group(ItemGroup.MISC).maxStackSize(16)));
+            () -> new BobRocksItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<BlastRockItem> BLAST_ROCK = ITEMS.register("blastrock",
-            () -> new BlastRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new BlastRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<AndesiteRockItem> ANDESITE_ROCK = ITEMS.register("andesiterock",
-            () -> new AndesiteRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new AndesiteRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<BasaltRockItem> BASALT_ROCK = ITEMS.register("basaltrock",
-            () -> new BasaltRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new BasaltRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<DioriteRockItem> DIORITE_ROCK = ITEMS.register("dioriterock",
-            () -> new DioriteRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new DioriteRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<GraniteRockItem> GRANITE_ROCK = ITEMS.register("graniterock",
-            () -> new GraniteRockItem(new Item.Properties().group(ItemGroup.COMBAT).maxStackSize(16)));
+            () -> new GraniteRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(16)));
     public static final RegistryObject<PetRockItem> PET_ROCK = ITEMS.register("petrock",
-            () -> new PetRockItem(new Item.Properties().group(ItemGroup.MISC).maxStackSize(1)));
+            () -> new PetRockItem(new Item.Properties().group(RockMod.ROCK_GROUP).maxStackSize(1)));
 
 }
