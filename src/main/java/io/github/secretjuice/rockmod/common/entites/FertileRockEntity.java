d464b75e92d5cd9047a25dc629469ec2c4ea9b08
SecretJuice <secret.juice33@gmail.com>
2021-02-11 11:05:42 -0600

    Added Bone mealing to Fertile Rock

--- src/main/java/io/github/secretjuice/rockmod/common/entites/FertileRockEntity.java
+++ src/main/java/io/github/secretjuice/rockmod/common/entites/FertileRockEntity.java
@@ -1,13 +1,19 @@
 package io.github.secretjuice.rockmod.common.entites;
 
+import io.github.secretjuice.rockmod.client.render.particle.ProjectileImpactParticleRenderer;
 import io.github.secretjuice.rockmod.core.damagesources.RockDamageSource;
 import io.github.secretjuice.rockmod.core.init.EntityTypeInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.IGrowable;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 public class FertileRockEntity extends RockEntity{
 
@@ -26,4 +32,49 @@ public class FertileRockEntity extends RockEntity{
     private BlockState particleBlockState = Blocks.OAK_LEAVES.getDefaultState();
     private DamageSource entityDamageSource = RockDamageSource.FERTILE_ROCK;
 
+    @Override
+    protected void onImpact(RayTraceResult result) {
+        super.onImpact(result);
+        if (!this.world.isRemote) {
+            this.world.setEntityState(this, (byte)3);
+            ProjectileImpactParticleRenderer.renderImpactParticles(this, getParticleBlockState());
+            boneMealArea();
+            this.remove();
+        }
+
+    }
+
+    protected void boneMealArea(){
+        BlockPos entityPos = this.getPosition();
+
+        BlockPos checkingPos = new BlockPos(entityPos.getX() - 1, entityPos.getY() - 1, entityPos.getZ() - 1);
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+
+                for (int k = 0; k < 3; k++){
+
+                    BlockState blockstate = world.getBlockState(checkingPos);
+
+                    if (blockstate.getBlock() instanceof IGrowable){
+                        IGrowable igrowable = (IGrowable)blockstate.getBlock();
+
+                        if (igrowable.canGrow(world, checkingPos, blockstate, world.isRemote)) {
+
+                            if (world instanceof ServerWorld) {
+
+                                if (igrowable.canUseBonemeal(world, world.rand, checkingPos, blockstate)) {
+
+                                    igrowable.grow((ServerWorld)world, world.rand, checkingPos, blockstate);
+                                }
+                            }
+                        }
+                    }
+                    checkingPos = new BlockPos(checkingPos.getX() + 1, checkingPos.getY(), checkingPos.getZ());
+                }
+                checkingPos = new BlockPos(checkingPos.getX() - 3, checkingPos.getY(), checkingPos.getZ() + 1);
+            }
+            checkingPos = new BlockPos(checkingPos.getX(), checkingPos.getY() + 1, checkingPos.getZ() - 3);
+        }
+    }
 }
