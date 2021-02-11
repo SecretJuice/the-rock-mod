71c9db6c4dd6bab60055e1de3105bbe82d063b6a
SecretJuice <secret.juice33@gmail.com>
2021-02-10 18:53:31 -0600

    Added Strange Gravel loot table and texture

--- src/main/java/io/github/secretjuice/rockmod/core/init/BlockInit.java
+++ src/main/java/io/github/secretjuice/rockmod/core/init/BlockInit.java
@@ -5,6 +5,7 @@ import io.github.secretjuice.rockmod.common.blocks.StrangeGravelBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,6 +15,6 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RockMod.MOD_ID);
 
     public static final RegistryObject<Block> STRANGE_GRAVEL = BLOCKS.register("strangegravel",
-            () -> new StrangeGravelBlock(AbstractBlock.Properties.from(Blocks.GRAVEL)));
+            () -> new StrangeGravelBlock(AbstractBlock.Properties.from(Blocks.GRAVEL).harvestTool(ToolType.SHOVEL)));
 
 }
